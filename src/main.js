561973c476d368ab83c25c3580e50b057d94a36f
TeiHek <lerandy1235@gmail.com>
2022-04-18 17:02:20 -0700

    Added Menu, play, temp player sprite

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu ],
+    scene: [ Menu, Play ],
     fps: {
         target: 60,
         forceSetTimeOut: true
     },
 }
+
+let game = new Phaser.Game(config);
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'left',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+      // show menu text
+      this.add.text(50, 20, 'Menu', menuConfig).setOrigin(0.5);
+    }
+
+    update() {
+
+    }
+}
